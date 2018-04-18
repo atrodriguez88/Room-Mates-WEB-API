ebd58c1b411c3f0a7c15c6e890b1330f72f02d9a
Ariel Triana <ariel.triana88@gmail.com>
2018-04-18 01:43:10 -0400

    Add Cors

--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,7 @@ namespace RoomM.API
                     .AddDefaultTokenProviders();
                     
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,7 @@ namespace RoomM.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(web => web.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
